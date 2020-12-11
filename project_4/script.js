5434a93ee38fd506f41e5cd6a772e5a9cb1ab776
D <d@diki.dev>
2020-12-11 13:18:50 +0000

    Corrected Proj No.

--- project_4/script.js
+++ project_4/script.js
@@ -1,25 +1,11 @@
-const loadText = document.querySelector('.loading-text');
-const bg = document.querySelector('.bg');
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
 
-let load = 0;
+const h1 = document.querySelector('.h1')
 
-let int = setInterval(blurring, 30);
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
 
-function blurring() {
-    load++;
-
-        if(load > 99) {
-            clearInterval(int);
-        }
-
-    loadText.innerHTML = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0);
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-    
-}
-
-/* Stackoverflow Reference for Function which maps loading time 0-100 and opactiy 1 to 0
-https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers */
-    const scale = (num, in_min, in_max, out_min, out_max) => {
-        return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-  }
